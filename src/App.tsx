92677fae791fbd27f52ab770dc69f8a2ce6c3f0c
ahmedmlaouhia <ahmedmlaouhia6@gmail.com>
2024-09-23 19:49:21 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { Toaster } from "react-hot-toast"
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+  ])
+  return (
+    <>
+      <Toaster />
+      <Navbar />
+      <RouterProvider router={router} />
+      <Footer />
+    </>
+  )
+}
+
+export default App
