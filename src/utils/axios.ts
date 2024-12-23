522b9afe9c6eb905333f9efcf6f5e1aa8ceac575
ahmedmlaouhia <ahmedmlaouhia6@gmail.com>
2024-12-23 16:06:08 +0100

    update

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 const client = () =>
   axios.create({
-    baseURL: import.meta.env.VITE_APP_API,
+    baseURL: import.meta.env.VITE_APP_API || "/api",
     headers: {
       "Content-Type": "application/json",
       token: localStorage.getItem("token"),
